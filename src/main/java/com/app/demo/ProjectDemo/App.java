85bf6cfbe4f9fed3803960f06fd3943e19c81c42
sanumula003 <sanumula003@gmail.com>
2023-11-22 13:10:27 +0530

    Demo for Team

---
+++ src/main/java/com/app/demo/ProjectDemo/App.java
@@ -0,0 +1,13 @@
+package com.app.demo.ProjectDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
